5ce4fe59039a6f63943c56ee1616808ad466e4c3
Nasir <nasirbhatti.nk@gmail.com>
2023-11-25 16:59:55 +0500

    Add success message when submit the form

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const form = document.querySelector('#form');
-
+const newsletterCard = document.querySelector('.newsletter-card');
+const success = document.querySelector('.success-card');
 
 function validateEmail(input) {
-    const email = input.value;
-    const errorMsg = document.querySelector('.error-msg');
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = input.value;
+  const errorMsg = input.nextElementSibling;
 
-    if(emailPattern.test(email)) {
-        errorMsg.style.display = 'none';
-        return true;
-    } else {
-        errorMsg.style.display = 'flex';
-        return false
-    }
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailPattern.test(email)) {
+    errorMsg.style.display = 'none';
+    return true;
+  } else {
+    errorMsg.style.display = 'flex';
+    return false;
+  }
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const input = document.querySelector('input');
-    if(validateEmail(input)) {
-        form.submit();
-        input.value = '';
-    }
-})
+  e.preventDefault();
+  const input = document.querySelector('input');
+
+  if (validateEmail(input)) {
+    success.style.display = 'flex'; // Display the success message
+    newsletterCard.style.display = 'none'; // Hide the form
+    input.value = ''; // Clear the input field
+  }
+});
+
+// Dismiss the success message when the "Dismiss message" button is clicked
+success.querySelector('button').addEventListener('click', () => {
+  success.style.display = 'none'; // Hide the success message
+  newsletterCard.style.display = 'flex'; // Show the form again
+});
